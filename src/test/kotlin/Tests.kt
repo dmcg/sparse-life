4e3109939b574081ef8d488e2d26fedbecd62ca9
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-11-10 09:54:29 +0000

    TCR on

--- src/test/kotlin/Tests.kt
+++ src/test/kotlin/Tests.kt
@@ -4,8 +4,11 @@ class Tests {
 
     @Test
     fun `create board`() {
-        val board: Board = setOf(0 to 0, 0 to 1, 0 to 2)
+        val board: Board = boardOf(0 to 0, 0 to 1, 0 to 2)
     }
+
 }
 
 typealias Board = Set<Pair<Int, Int>>
+
+private fun boardOf(vararg liveCells: Pair<Int, Int>) = setOf(*liveCells)
