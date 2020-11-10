acafa5711abed59ec7e53ab1806d5c2b1998bc0c
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-11-10 10:00:52 +0000

    TCR on

--- src/test/kotlin/Tests.kt
+++ src/test/kotlin/Tests.kt
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class Tests {
@@ -15,9 +16,16 @@ class Tests {
         assertTrue(next.contains(0 to 1))
     }
 
+    @Test
+    fun `cell with 1 neighbour dies`() {
+        val board: Board = boardOf(0 to 0, 0 to 1, 0 to 2)
+        val next: Board = board.step()
+        assertEquals(next, setOf(0 to 1))
+    }
 }
+
+fun Board.step(): Board = setOf(0 to 1)
 
 
 typealias Board = Set<Pair<Int, Int>>
 fun boardOf(vararg liveCells: Pair<Int, Int>) = setOf(*liveCells)
-fun Board.step(): Board = this
